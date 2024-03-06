824c771361e9517a4b8a6cb64375596d0026e940
Ousmane <98632155+Ousmane-T@users.noreply.github.com>
2024-03-05 21:05:34 -0500

    Message de commit

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE band ( bandName varchar(50),
+                    creation YEAR,
+                    genre varchar(50),
+                    PRIMARY KEY (bandName));
+
+INSERT INTO band (bandName, creation, genre) VALUES ("Crazy Duo", 2015, "rock"), ("Mysterio", 2019, "pop"), ("Luna", 2009, "classical");
+ALTER TABLE singer RENAME TO musician;
+
+ALTER TABLE musician CHANGE COLUMN singerName musicianName varchar(50);
+
+ALTER TABLE musician ADD COLUMN role varchar(50);
+
+ALTER TABLE musician ADD COLUMN bandName varchar(50);
+
+UPDATE musician SET role = "vocals", bandName = "Crazy Duo" WHERE musicianName = "Alina";
+
+UPDATE musician SET role = "guitar", bandName = "Mysterio" WHERE musicianName = "Mysterio";
+
+UPDATE musician SET role = "percussion", bandName = "Crazy Duo" WHERE musicianName = "Rainbow";
+
+UPDATE musician SET role = "piano", bandName = "Luna" WHERE musicianName = "Luna";
